e42818153cf317831d843f504ff81b7bee3ecd33
Skyler Burwell <skyboy49707@gmail.com>
2023-10-26 19:34:59 -0600

    Blank again.

--- main.cpp
+++ main.cpp
@@ -8,28 +8,11 @@
 
 #include <iostream>
 
+#include "game.hpp"
+
 int main() {
-    glfwInit();
-    
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(1920, 1200, "Vulkan window", nullptr, nullptr);
-    
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-    
-    std::cout << extensionCount << " extensions supported\n";
-    
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
-    
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
-    
-    glfwDestroyWindow(window);
-    
-    glfwTerminate();
+    Game game;
+    game.run();
     
     return 0;
 }
